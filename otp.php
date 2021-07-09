80cf9ed9a96c12198612d0f5f50c6fc135a08655
Ulaganathan <ulagadhoni.25@gmail.com>
2021-07-09 11:44:19 +0530

    removed email credentials from otp.php

--- otp.php
+++ otp.php
@@ -59,8 +59,8 @@ $mail = new PHPMailer;
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'ulagadhoni.25@gmail.com';                 // SMTP username
-$mail->Password = 'Ulga777@';                           // SMTP password
+$mail->Username = 'Your email id';                 // SMTP username
+$mail->Password = 'Email Password';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
