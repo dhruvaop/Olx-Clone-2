20d06325eeebe01cf0740041e684361d6ef2dfe7
Ulaganathan <ulagadhoni.25@gmail.com>
2021-07-15 00:27:58 +0530

    AJAX for post liking in product info page finished

--- product.php
+++ product.php
@@ -33,6 +33,16 @@ $p_id=$_GET['id'];
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
     crossorigin="anonymous">
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
+  
+
+<script>
+    function myfav(product_id) {
+      var check = document.getElementById(product_id).checked ? '1' : '0';
+     
+      $.post('ajax.php', { checked: check, p_id: product_id });
+
+    }
+  </script>
 
   
   <style>
